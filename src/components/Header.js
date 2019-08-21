266e1978b96e8bc58bd104ec9ce28ded66e572d5
Rohan <rohanbhatia96@outlook.com>
2019-08-22 01:54:23 +0530

    Initial Styling Bug Fixes

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header align="center">
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="Problem?" 
+                width="20%"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
