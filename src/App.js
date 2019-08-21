ad9bf977594179cf6722faf667d12304c9dc42c2
Rohan <rohanbhatia96@outlook.com>
2019-08-22 01:56:34 +0530

    Revert "Initial Styling Bug Fixes"
    This reverts commit 266e1978b96e8bc58bd104ec9ce28ded66e572d5.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from './components/Header'
+
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="App">
+      Hello World!
     </div>
   );
 }
