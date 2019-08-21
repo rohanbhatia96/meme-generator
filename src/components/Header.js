ccdbb9506a6c6ed323efe92fc3476a789adbcb6d
Rohan <rohanbhatia96@outlook.com>
2019-08-22 02:10:36 +0530

    Added MemeGenerator Code + Shifted style.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,10 @@ import React from "react"
 
 function Header() {
     return (
-        <header align="center">
+        <header>
             <img 
                 src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
                 alt="Problem?"
-                width="20%"
             />
             <p>Meme Generator</p>
         </header>
